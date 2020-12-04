c7ac30a41623f1d041f57e4fc677318541dc15fb
CrescentVelvet <crescentvelvet3@gmail.com>
2020-12-04 17:57:05 +0800

    修改查询数据与清零指令

--- image_draw.py
+++ image_draw.py
@@ -88,11 +88,13 @@ if image_flag.sim_flag == 0:
     # COM_NUM = input('Please input an order number to choose a COM:')
     # ser = serial.Serial(list(port_list[COM_NUM])[0], bps, timeout=time_out)
     ser = serial.Serial(list(port_list[0])[0], bps, timeout=time_out)
-    cmd_query_data = bytes.fromhex('fa f5 01 02 00 00 0e fe') # 查询数据
+    # cmd_query_data = bytes.fromhex('fa f5 01 02 00 00 0e fe') # 查询数据
+    cmd_query_data = bytes.fromhex('01 0x32 00 00 33') # 查询数据修改
     cmd_query_param = bytes.fromhex('fa f5 01 01 00 00 0f fe') # 查询参数
     cmd_enable_MCA = bytes.fromhex('fa f5 01 00 00 00 10 fe') # enable_MCA
     cmd_disable_MCA = bytes.fromhex('fa f5 02 00 00 00 0f fe') # disable_MCA
-    cmd_query_data_and_clear = bytes.fromhex('fa f5 02 02 00 00 0d fe') # 查询数据并清零
+    # cmd_query_data_and_clear = bytes.fromhex('fa f5 02 02 00 00 0d fe') # 查询数据并清零
+    cmd_query_data_and_clear = bytes.fromhex('01 0x33 00 00 34') # 查询数据并清零修改
     cmd_query_data_and_param = bytes.fromhex('fa f5 03 02 00 00 0c fe') # 查询数据和参数
     cmd_query_data_and_param_and_clear = bytes.fromhex('fa f5 04 02 00 00 0b fe') # 查询数据和参数并清零数据
     
